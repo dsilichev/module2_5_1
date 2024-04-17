2803bfb15213d845b3965123812c3cb3686d6746
Dmitry <dsilichev@gmail.com>
2024-04-17 16:12:41 +0300

    jsonPlaceholder todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import styles from './app.module.css';
+import { TodoList } from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setTodos([...json].slice(0, 10)))
+      .finally();
+  }, []);
   
   return (
-    <div className={styles.App}>
-      <h1>Todo List</h1>
+    <div className={styles.app}>
+      <TodoList todos={todos}/>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,17 @@
-export const TodoList = () => {
+import styles from '../app.module.css';
+
+export const TodoList = ({ todos }) => {
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Todo list</h1>
+      <ul className={styles.list}>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" id={todo.id} defaultChecked={todo.completed}></input>
+            <label htmlFor={todo.id}>{todo.title}</label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
